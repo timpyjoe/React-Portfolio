e523383500cabdfdedf2802ddb7f3b5eb17d3507
Tim Poffenberger <timpoffenberger23@gmail.com>
2023-12-07 09:52:15 -0600

    Adds assets folder and begins building out portfolio page

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,4 +1,4 @@
-
+import Project from "./Project"
 
 export default function Portfolio() {
 
@@ -6,6 +6,15 @@ export default function Portfolio() {
   return(
     <>
       <h2>Projects</h2>
+      <div className="projects">
+        
+        <Project imgPath="../../assets/Unblurrd.png" title="Unblurrd: Game Review" gitLink="https://github.com/VictorMontelongo/Project-2-Video-Game-Reviews"/>
+        <Project />
+        <Project />
+        <Project />
+        <Project />
+        <Project />
+      </div>
     
     
     
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,14 +1,32 @@
+import {useState} from "react"
 
 
+export default function Project({ imgPath, title, gitLink, deployLink}){
 
-export default function Project(){
-
+  const [ isShown, setIsShown ] = useState(false)
 
 
   return(
-    <>
+    <div className="projectItem" >
+      <a href={deployLink} target="_blank">
+      <img src={imgPath} alt={title}  
+      onMouseEnter={() => setIsShown(true)}
+      onMouseLeave={() => setIsShown(false)}
+      />
+      </a>
+      {/* {isShown && (
+        <div className="content">
+          I appear when Shown
+        </div>
+      )} */}
+      <p>{title}</p>
+      <a href={gitLink} target="_blank">
+        <i className="fa-brands fa-github" />
+      </a>
+
+      
 
 
-    </>
+    </div>
   )
 }
