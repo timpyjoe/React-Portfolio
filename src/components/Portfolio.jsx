7dc7c6cd3e36740b19991922ef3a7ce49b3afcf0
Tim Poffenberger <timpoffenberger23@gmail.com>
2023-12-07 19:49:00 -0600

    changes img locations and file paths to reflect

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -8,11 +8,11 @@ export default function Portfolio() {
       <h2>Projects</h2>
       <div className="projects">
         
-        <Project imgPath="../../assets/Unblurrd.png" title="Unblurrd: Game Review" gitLink="https://github.com/VictorMontelongo/Project-2-Video-Game-Reviews" deployLink="https://video-game-review-eb186ee09616.herokuapp.com/"/>
-        <Project imgPath="../../assets/Hockey-Trivia.png" title="Hockey Trivia Game" gitLink="https://github.com/mhalder4/group-project-1" deployLink=""/>
-        <Project imgPath="../../assets/Note Taker.png" title="Note Taking App" gitLink="https://github.com/timpyjoe/Notes-Taker" deployLink="https://notes-taker-app-94f83f45691b.herokuapp.com/notes"/>
-        <Project imgPath="../../assets/Code-stock.jpg" title="README Generator" gitLink="https://github.com/timpyjoe/README-generator" deployLink="https://drive.google.com/file/d/1_sHoPDzMugQD8IhuGO3O0A0LnQhUoIQu/view"/>
-        <Project imgPath="" title="" gitLink="" deployLink=""/>
+        <Project imgPath="../../Unblurrd.png" title="Unblurrd: Game Review" gitLink="https://github.com/VictorMontelongo/Project-2-Video-Game-Reviews" deployLink="https://video-game-review-eb186ee09616.herokuapp.com/"/>
+        <Project imgPath="../../Hockey-Trivia.png" title="Hockey Trivia Game" gitLink="https://github.com/mhalder4/group-project-1" deployLink=""/>
+        <Project imgPath="../../Note Taker.png" title="Note Taking App" gitLink="https://github.com/timpyjoe/Notes-Taker" deployLink="https://notes-taker-app-94f83f45691b.herokuapp.com/notes"/>
+        <Project imgPath="../../Code-stock.jpg" title="README Generator" gitLink="https://github.com/timpyjoe/README-generator" deployLink="https://drive.google.com/file/d/1_sHoPDzMugQD8IhuGO3O0A0LnQhUoIQu/view"/>
+        <Project imgPath="../../portfolio.png" title="Portfolio Webpage" gitLink="https://github.com/timpyjoe/React-Portfolio" deployLink=""/>
         <Project imgPath="" title="" gitLink="" deployLink=""/>
       </div>
     
