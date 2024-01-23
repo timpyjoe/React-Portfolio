950c4793c9f6c76b07777a7f7692cfcb71b481f4
Tim Poffenberger <timpoffenberger23@gmail.com>
2024-01-22 23:00:21 -0600

    Updates About Me page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,14 +11,14 @@ export default function About(){
 
       <h2>About Me</h2>
       <p>
-        Hello, my name is Tim and I am a Junior Full-Stack Web Developer. I'm passionate about learning new things, and love trying to find new and interesting ways to incorporate previous experiences into current and future projects. I'm well rounded in my skills and bring what I've learned from my craft to give projects my own personal style.
+        Hello, my name is Tim and I am a Junior Full-Stack Web Developer. I'm passionate about learning new things, and love trying to find new and interesting ways to incorporate previous experiences into current and future projects. I'm well rounded in my skills and bring what I've learned from my craft to give projects my own personal style. This portfolio website was built entirely by me, so feel free to look around and see if anything catches your interest!
       </p>
       <p>
-        When I'm not working or coding, I enjoy cooking, playing video games, and spending time with cats. I try to travel as much as I can, with some of my favorite places to visit being San Diego, California and Aspen, Colorado. I am also almost always listening to music, and see my favorite bands live in concert whenever I have the opportunity.
+        When I'm not working or coding, I enjoy cooking, playing video games, and spending time with cats. I try to travel as much as I can, with some of my favorite places to visit being San Diego, California and Aspen, Colorado. I am almost always listening to music, and see my favorite bands live in concert whenever I have the opportunity.
       </p>
         </Col>
         <Col sm={12} md={6}>
-          <img src={"../../Profile Pic.png"} alt={"Picture of Tim Poffenberger"}/>
+          <img src={"../../Profile Pic.jpg"} alt={"Picture of Tim Poffenberger"} style={{width: "100%", paddingBottom: "0.6em"}}/>
         </Col>
       </Row>
     
