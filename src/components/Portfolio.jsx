10e68189d8879b725fcac3166781d3dc911c58f8
Tim Poffenberger <timpoffenberger23@gmail.com>
2024-01-30 23:24:47 -0600

    Some formatting on portfolio page

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,4 +1,5 @@
 import Project from "./Project"
+import { Col, Row } from "react-bootstrap"
 
 export default function Portfolio() {
 
@@ -7,13 +8,30 @@ export default function Portfolio() {
     <>
       <h2>Projects</h2>
       <div className="projects">
-        
-        <Project imgPath="../../gifty.jpg" title="Gifty: Gift Idea Tracker" gitLink="https://github.com/timpyjoe/Gift-idea-tracker" deployLink="https://gifty-gift-idea-tracker-9346ec5c500d.herokuapp.com/"/>
-        <Project imgPath="../../Unblurrd.png" title="Unblurrd: Game Review" gitLink="https://github.com/VictorMontelongo/Project-2-Video-Game-Reviews" deployLink="https://video-game-review-eb186ee09616.herokuapp.com/"/>
+        <Row>
+          <Col sm={12} md={6}>
+          <Project imgPath="../../gifty.jpg" title="Gifty: Gift Idea Tracker" gitLink="https://github.com/timpyjoe/Gift-idea-tracker" deployLink="https://gifty-gift-idea-tracker-9346ec5c500d.herokuapp.com/"/>
+          </Col>
+          <Col sm={12} md={6}>
+          <Project imgPath="../../Unblurrd.png" title="Unblurrd: Game Review" gitLink="https://github.com/VictorMontelongo/Project-2-Video-Game-Reviews" deployLink="https://video-game-review-eb186ee09616.herokuapp.com/"/>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={12} md={6}>
         <Project imgPath="../../Hockey-Trivia.png" title="Hockey Trivia Game" gitLink="https://github.com/mhalder4/group-project-1" deployLink=""/>
+          </Col>
+          <Col sm={12} md={6}>
         <Project imgPath="../../Note Taker.png" title="Note Taking App" gitLink="https://github.com/timpyjoe/Notes-Taker" deployLink="https://notes-taker-app-94f83f45691b.herokuapp.com/notes"/>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={12} md={6}>
         <Project imgPath="../../Code-stock.jpg" title="README Generator" gitLink="https://github.com/timpyjoe/README-generator" deployLink="https://drive.google.com/file/d/1_sHoPDzMugQD8IhuGO3O0A0LnQhUoIQu/view"/>
+          </Col>
+          <Col sm={12} md={6}>
         <Project imgPath="../../portfolio.png" title="Portfolio Webpage" gitLink="https://github.com/timpyjoe/React-Portfolio" deployLink="https://main--hilarious-torte-26d0ae.netlify.app/portfolio"/>
+          </Col>
+        </Row>
       </div>
     
     
